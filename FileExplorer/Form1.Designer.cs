431c55261bb813355d672a377d2dc47123991b75
SoftStar <qq764073542@163.com>
2015-10-28 19:45:35 +0800

    FileExplorer
    文件资源管理器
    状态栏显示
    文件（文本文件、图片文件预览）

---
+++ FileExplorer/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView_list = new System.Windows.Forms.TreeView();
+            this.textBox_path = new System.Windows.Forms.TextBox();
+            this.button_open = new System.Windows.Forms.Button();
+            this.listView_show = new System.Windows.Forms.ListView();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.button_back = new System.Windows.Forms.Button();
+            this.richTextBox_txtShow = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_copy = new System.Windows.Forms.Button();
+            this.button_cut = new System.Windows.Forms.Button();
+            this.button_parse = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button_go = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView_list
+            // 
+            this.treeView_list.Location = new System.Drawing.Point(12, 68);
+            this.treeView_list.Name = "treeView_list";
+            this.treeView_list.Size = new System.Drawing.Size(232, 626);
+            this.treeView_list.TabIndex = 0;
+            this.treeView_list.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_list_AfterSelect);
+            // 
+            // textBox_path
+            // 
+            this.textBox_path.Location = new System.Drawing.Point(41, 12);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(753, 21);
+            this.textBox_path.TabIndex = 1;
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(769, 38);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 23);
+            this.button_open.TabIndex = 2;
+            this.button_open.Text = "打开";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // listView_show
+            // 
+            this.listView_show.Location = new System.Drawing.Point(250, 68);
+            this.listView_show.Name = "listView_show";
+            this.listView_show.Size = new System.Drawing.Size(622, 363);
+            this.listView_show.TabIndex = 3;
+            this.listView_show.UseCompatibleStateImageBehavior = false;
+            this.listView_show.View = System.Windows.Forms.View.List;
+            this.listView_show.SelectedIndexChanged += new System.EventHandler(this.listView_show_SelectedIndexChanged_1);
+            // 
+            // button_back
+            // 
+            this.button_back.Location = new System.Drawing.Point(12, 39);
+            this.button_back.Name = "button_back";
+            this.button_back.Size = new System.Drawing.Size(75, 23);
+            this.button_back.TabIndex = 4;
+            this.button_back.Text = "后退";
+            this.button_back.UseVisualStyleBackColor = true;
+            this.button_back.Click += new System.EventHandler(this.button_back_Click);
+            // 
+            // richTextBox_txtShow
+            // 
+            this.richTextBox_txtShow.Location = new System.Drawing.Point(250, 437);
+            this.richTextBox_txtShow.Name = "richTextBox_txtShow";
+            this.richTextBox_txtShow.Size = new System.Drawing.Size(622, 257);
+            this.richTextBox_txtShow.TabIndex = 5;
+            this.richTextBox_txtShow.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(398, 437);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 257);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button_copy
+            // 
+            this.button_copy.Location = new System.Drawing.Point(148, 39);
+            this.button_copy.Name = "button_copy";
+            this.button_copy.Size = new System.Drawing.Size(75, 23);
+            this.button_copy.TabIndex = 7;
+            this.button_copy.Text = "复制";
+            this.button_copy.UseVisualStyleBackColor = true;
+            this.button_copy.Click += new System.EventHandler(this.button_copy_Click);
+            // 
+            // button_cut
+            // 
+            this.button_cut.Location = new System.Drawing.Point(292, 40);
+            this.button_cut.Name = "button_cut";
+            this.button_cut.Size = new System.Drawing.Size(75, 23);
+            this.button_cut.TabIndex = 8;
+            this.button_cut.Text = "剪切";
+            this.button_cut.UseVisualStyleBackColor = true;
+            this.button_cut.Click += new System.EventHandler(this.button_cut_Click);
+            // 
+            // button_parse
+            // 
+            this.button_parse.Enabled = false;
+            this.button_parse.Location = new System.Drawing.Point(448, 39);
+            this.button_parse.Name = "button_parse";
+            this.button_parse.Size = new System.Drawing.Size(75, 23);
+            this.button_parse.TabIndex = 9;
+            this.button_parse.Text = "粘贴";
+            this.button_parse.UseVisualStyleBackColor = true;
+            this.button_parse.Click += new System.EventHandler(this.button_parse_Click);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(619, 38);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(75, 23);
+            this.button_delete.TabIndex = 10;
+            this.button_delete.Text = "删除";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 694);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(887, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(0, 17);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.StatuUpdate);
+            // 
+            // button_go
+            // 
+            this.button_go.Location = new System.Drawing.Point(801, 12);
+            this.button_go.Name = "button_go";
+            this.button_go.Size = new System.Drawing.Size(75, 23);
+            this.button_go.TabIndex = 12;
+            this.button_go.Text = "转到";
+            this.button_go.UseVisualStyleBackColor = true;
+            this.button_go.Click += new System.EventHandler(this.button_go_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button_go;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(887, 716);
+            this.Controls.Add(this.button_go);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.button_parse);
+            this.Controls.Add(this.button_cut);
+            this.Controls.Add(this.button_copy);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox_txtShow);
+            this.Controls.Add(this.button_back);
+            this.Controls.Add(this.listView_show);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.textBox_path);
+            this.Controls.Add(this.treeView_list);
+            this.Name = "Form1";
+            this.Text = "资源管理器";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView_list;
+        private System.Windows.Forms.TextBox textBox_path;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.ListView listView_show;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button button_back;
+        private System.Windows.Forms.RichTextBox richTextBox_txtShow;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_copy;
+        private System.Windows.Forms.Button button_cut;
+        private System.Windows.Forms.Button button_parse;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button_go;
+    }
+}
+
---
+++ FileExplorer/Form1.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 定义初始的全局变量
+        /// </summary>
+        string explorerPath = "";
+        string treeViewPath = "";
+        /// <summary>
+        /// 点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_open_Click(object sender, EventArgs e)
+        {
+           
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string path = folderBrowserDialog.SelectedPath;
+                    explorerPath = path;
+                    treeViewPath = path;
+                    this.textBox_path.Text = path;
+                    this.treeView_list.Nodes.Clear();
+                    getExplorerView(null, path);
+                }
+         
+          
+        }
+        /// <summary>
+        /// 单层遍历，仅显示当前目录下的文件夹和文件
+        /// </summary>
+        /// <param name="path"></param>
+        private void getFolderView(string path)
+        {
+            explorerPath = path;
+            textBox_path.Text = explorerPath;
+            try
+            {
+                this.listView_show.Items.Clear();
+                DirectoryInfo TheFolder = new DirectoryInfo(path);
+                //遍历文件夹
+                foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories())
+                {
+                    this.listView_show.Items.Add("【" + NextFolder.Name + "】");
+
+                }
+                //遍历文件
+                foreach (FileInfo NextFile in TheFolder.GetFiles())
+                    this.listView_show.Items.Add(NextFile.Name); //再添加文件大小显示
+            }
+            catch(Exception)
+            {
+
+            }
+           
+        }       
+        /**
+         * 遍历方案： -- 深度优先遍历
+         * 1.获取当前目录下的所有文件夹
+         * 2.将第一个目录添加到treeView节点，并返回该节点的 TreeNode对象 
+         * 3.递归将返回到treeNode对象传递进去，还有子级目录的文件夹名称
+         * 4.循环
+         * 5.深度结束条件：子级目录为空 
+         * 问题，第一层根节点怎么传值？ 解决 传一个 null
+         */ 
+
+        /// <summary>
+        /// 遍历函数
+        /// </summary>
+        /// <param name="path"></param>        
+        private void getExplorerView(TreeNode node,string path)
+        {
+            try
+            {
+                TreeNode newnode = null;
+                DirectoryInfo TheFolder = new DirectoryInfo(path);
+                //遍历文件夹
+                foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories())
+                {
+                    newnode = addTreeViewLevelNode(node, NextFolder.Name); //添加一个节点
+                    getExplorerView(newnode, NextFolder.FullName);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("路径不存在");
+            }
+
+           
+        }
+
+        /// <summary>
+        /// 目录遍历函数
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private TreeNode addTreeViewLevelNode(TreeNode node, string name)
+        {
+            TreeNode levelNode = new TreeNode();
+            levelNode.Text = name;
+            if (node == null)
+            {
+                treeView_list.Nodes.Add(levelNode);
+            }
+            else
+            {              
+                levelNode.Text = name;
+                node.Nodes.Add(levelNode);
+            }
+            return levelNode;
+        }
+        /// <summary>
+        /// treeView节点点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>          
+        private void treeView_list_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string newPath = "";
+            TreeNode node = null;                   
+            try
+            {
+                node = e.Node.Parent;
+                newPath = e.Node.Text;   
+                while (true)
+                {
+                    newPath = newPath.Insert(0, node.Text + "\\"); //node.Text+"\\"                 
+                    node = node.Parent;        
+                }                
+            }
+            catch (NullReferenceException)
+            {
+           
+            }
+            if (!newPath.Equals(""))
+            {
+                explorerPath = newPath.Insert(0, treeViewPath + "\\");                            
+                getFolderView(explorerPath);//遍历该层文件 
+            }           
+        }
+        /// <summary>
+        /// 处理listView中目录点击展开事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+      
+        private void listView_show_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string filename = this.listView_show.SelectedItems[0].SubItems[0].Text;
+                if (filename.StartsWith("【")) //目录处理方式
+                {
+                    filename = filename.Replace("【", "");
+                    filename = filename.Replace("】", "");
+                    string newPath = explorerPath + "\\" + filename;
+                    getFolderView(newPath);//遍历该层文件                    
+                }
+                else //文件处理方式
+                {                 
+                    string newPath = explorerPath + "\\" + filename;    
+                    //显示文件大小
+                    int size = getFileSize(newPath);
+                    if(size > 1024*1024)
+                    {
+                        toolStripStatusLabel3.Text = ((float)size / (1024 * 1024)).ToString("F1") +"GB";
+                    }
+                    else if (size>1024)
+                    {
+                        toolStripStatusLabel3.Text = ((float)size / 1024).ToString("F1") + "MB";
+                    }
+                    else
+                    {
+                        toolStripStatusLabel3.Text = size + "KB";
+                         
+                    }
+                    OpenFile(newPath); 
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                //MessageBox.Show(ex.ToString());
+            }          
+           
+        }
+        /// <summary>
+        /// 后退按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_back_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string newPath = explorerPath.Substring(0, explorerPath.LastIndexOf("\\"));
+                getFolderView(newPath);
+            }catch(ArgumentOutOfRangeException)
+            {
+            }           
+        }
+        /// <summary>
+        /// 打开文件，仅支持文本文件，而且文件大小小于1MB
+        /// </summary>
+        /// <param name="path"></param>
+        private void OpenFile(string path)
+        {            
+            if(getFileSize(path)<1000 && getFileType(path)) //如果文件小于1000KB，并且后缀为文本文件类型
+            {                
+                Read_line(path);
+            }
+            else if (getFileSize(path) < 5000 && getPicFileType(path))
+            {
+                openPic(path);
+            }
+        }
+        /// <summary>
+        /// 读取文本文件 - 自动换行
+        /// </summary>
+        /// <param name="path"></param>
+        public void Read_line(string path)
+        {
+            this.richTextBox_txtShow.Visible = true;
+            this.pictureBox1.Visible = false;
+            this.richTextBox_txtShow.Clear();
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                this.richTextBox_txtShow.Text += line.ToString() + "\n";
+            }
+            sr.Close();
+        }
+        /// <summary>
+        /// 打开一幅图像，前提是先将之前的文本控件隐藏，然后显示图片控件
+        /// </summary>
+        /// <param name="path"></param>
+        private void openPic(string path)
+        {
+            this.richTextBox_txtShow.Visible = false;
+            this.pictureBox1.Visible = true;           
+            this.pictureBox1.ImageLocation = path;
+        }
+        /// <summary>
+        /// 获取文件的大小
+        /// </summary>
+        /// <param name="path"></param>
+        private int getFileSize(string path)
+        {
+            try
+            {
+                FileInfo fi = new FileInfo(path); //返回的是字节大小
+                float x = ((float)fi.Length / 1024);
+                int result = (int)(x + 1);
+                return result;
+            }
+            catch (FileNotFoundException)
+            {
+                return 0;
+            }
+            
+        }
+        /// <summary>
+        /// 获取文件类型是否是文本类型，主要是判断文件后缀。
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private bool getFileType(string path)
+        {
+           
+            string fileType = path.Substring(path.LastIndexOf(".")+1, path.Length - path.LastIndexOf(".") -1);
+            string[] type = { "txt", "xml", "ini", "conf", "java", "cs", "sql", "html", "js", "css", "c", "h", "cpp","py" };
+
+            for (int i = 0; i < type.Length;i++ )
+            {
+                if (fileType.Equals(type[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 获取文件类型是否为图片，主要是根据文件后缀判断
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private bool getPicFileType(string path)
+        {
+            string fileType = path.Substring(path.LastIndexOf(".") + 1, path.Length - path.LastIndexOf(".") - 1);
+            string[] type = { "jpg", "jpeg", "png", "bmp", "ico", "gif" };
+            for (int i = 0; i < type.Length; i++)
+            {
+                if (fileType.Equals(type[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 定时器-状态栏
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StatuUpdate(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToString();//时间
+            toolStripStatusLabel2.Text = "共"+this.listView_show.Items.Count+"个项目"; //文件数          
+        }
+        /// <summary>
+        /// 转到
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_go_Click(object sender, EventArgs e)
+        {
+            string path = this.textBox_path.Text;
+            explorerPath = path;
+            treeViewPath = path;            
+            this.treeView_list.Nodes.Clear();
+            getExplorerView(null, path);
+        }
+        /// <summary>
+        /// 文件删除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filename = this.listView_show.SelectedItems[0].SubItems[0].Text;
+                string newPath = explorerPath + "\\" + filename;
+                if (!filename.Equals(""))
+                {
+                    DialogResult r1 = MessageBox.Show("是否永久删除该文件？", "删除", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (r1.ToString() == "Yes")
+                    {
+                        //删除文件                   
+                        File.Delete(newPath);
+                        getFolderView(explorerPath);
+                    }
+
+                    else if (r1.ToString().Equals("No"))
+                    {
+                        return;
+                    }
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("暂时不允许删除目录");
+            }
+            
+        }
+        /// <summary>
+        /// 拷贝按钮点击事件,目前仅针对文件，文件夹不可以
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        string copy_path = "";
+        string copy_name = "";
+        private void button_copy_Click(object sender, EventArgs e)
+        {
+            this.button_parse.Enabled = true;
+            copy_name = this.listView_show.SelectedItems[0].SubItems[0].Text;
+            copy_path = explorerPath + "\\" + copy_name;
+        }
+        /// <summary>
+        /// 剪切按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        string cut_path = "";
+        string cut_name = "";
+        private void button_cut_Click(object sender, EventArgs e)
+        {
+            this.button_parse.Enabled = true;
+            cut_name = this.listView_show.SelectedItems[0].SubItems[0].Text;
+            cut_path = explorerPath + "\\" + cut_name;
+        }
+        /// <summary>
+        /// 粘贴按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        string parse_path = "";
+        private void button_parse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                parse_path = explorerPath;
+                if (!copy_path.Equals(""))
+                {
+                    File.Copy(copy_path, parse_path + "\\" + copy_name);
+                    getFolderView(parse_path);
+                    copy_path = "";
+                }
+
+                else if (!cut_path.Equals(""))
+                {
+                    File.Move(cut_path, parse_path + "\\" + cut_name);
+                    getFolderView(parse_path);
+                    cut_path = "";
+                }
+            }catch(Exception)
+            {
+                MessageBox.Show("目前支持对文件操作");
+            }
+            
+           
+        }      
+
+    }
+}
